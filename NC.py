c2bad72b1f62fbb4c7e9e97a202b1404b01bad80
VAMOT <tianhao.duan@ucdconnect.ie>
2024-01-11 08:08:02 +0800

    version3

---
+++ NC.py
@@ -0,0 +1,79 @@
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from keras.preprocessing.text import Tokenizer
+from keras.optimizers import Adam
+from keras.losses import SparseCategoricalCrossentropy
+from NCModel import NCModel
+import re
+import pandas as pd
+
+# Stopwords list from https://github.com/Yoast/YoastSEO.js/blob/develop/src/config/stopwords.js
+stopwords = ["a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "as", "at",
+             "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "could", "did", "do",
+             "does", "doing", "down", "during", "each", "few", "for", "from", "further", "had", "has", "have", "having",
+             "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how",
+             "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "it", "it's", "its", "itself",
+             "let's", "me", "more", "most", "my", "myself", "nor", "of", "on", "once", "only", "or", "other", "ought",
+             "our", "ours", "ourselves", "out", "over", "own", "same", "she", "she'd", "she'll", "she's", "should",
+             "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then",
+             "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through",
+             "to", "too", "under", "until", "up", "very", "was", "we", "we'd", "we'll", "we're", "we've", "were",
+             "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why",
+             "why's", "with", "would", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself",
+             "yourselves"]
+
+
+# removing non alphanumeric character
+def alpha_num(text):
+    return re.sub(r'[^A-Za-z0-9 ]', '', text)
+
+
+# removing the stopwords from text
+def remove_stopwords(text):
+    final_text = []
+    for i in text.split():
+        if i.strip().lower() not in stopwords:
+            final_text.append(i.strip())
+    return " ".join(final_text)
+
+
+def preprocess(df):
+    df['title'] = df['title'].str.lower()
+    df['title'] = df['title'].apply(alpha_num)
+    df['title'] = df['title'].apply(remove_stopwords)
+    return df
+
+
+train_df = pd.read_csv("./dataset/preprocessed_train.csv")
+# train_df = preprocess(train_df)
+# parameters
+vocab_size = 7000
+max_length = 200
+trunc_type = 'post'
+padding_type = 'post'
+oov_tok = "<OOV>"
+train_x, train_y = train_df['content'].to_numpy(), train_df['category'].to_numpy().reshape(len(train_df['category']), 1)
+# train-test split
+train_x, valid_x, train_y, valid_y = train_test_split(train_x, train_y, test_size=0.2, stratify=train_y, shuffle=True)
+# tokenize sentences
+# tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
+# tokenizer.fit_on_texts(train_x)
+
+# convert train dataset to sequence and pad sequences
+# train_x = tokenizer.texts_to_sequences(train_x)
+# train_x = tf.keras.preprocessing.sequence.pad_sequences(train_x, padding=padding_type, truncating=trunc_type, maxlen=max_length)
+
+# convert valid dataset to sequence and pad sequences
+# valid_x = tokenizer.texts_to_sequences(valid_x)
+# valid_x = tf.keras.preprocessing.sequence.pad_sequences(valid_x, padding=padding_type, truncating=trunc_type, maxlen=max_length)
+
+model = NCModel(vocab_size=vocab_size, max_length=max_length,vocab=train_x,classes=32)
+model.build((None,1))
+model.compile(loss=SparseCategoricalCrossentropy(),
+              optimizer=Adam(),
+              metrics=['accuracy'])
+model.summary()
+
+history = model.fit(train_x, train_y, batch_size=32, epochs=10)
+
+model.evaluate(valid_x, valid_y, return_dict=True)
--- NCModel.py
+++ NCModel.py
@@ -1,39 +1,40 @@
 import tensorflow as tf
 import keras
-from keras.layers import Conv1D, Dense, Embedding, GlobalAveragePooling1D, Activation, BatchNormalization
+from keras.layers import Conv1D, Dense, Embedding, GlobalAveragePooling1D, Activation, BatchNormalization, Layer, \
+    Dropout, LSTM, TextVectorization, Input
 
 
 class NCModel(keras.Model):
-    def __init__(self, input_dim, input_length, classes, *args, **kwargs):
+    def __init__(self, vocab_size, max_length,vocab,classes, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.eb = Embedding(input_dim=input_dim, output_dim=64,
-                            input_shape=(None,input_length), input_length=input_length)
-
-        self.cl1 = Conv1D(filters=128, kernel_size=3, strides=1, padding="same")
-        self.bn1 = BatchNormalization()
-        self.relu1 = Activation("relu")
-        self.cl2 = Conv1D(filters=128, kernel_size=3, strides=1, padding="same")
-        self.bn2 = BatchNormalization()
-        self.relu2 = Activation("relu")
-        self.cl3 = Conv1D(filters=128, kernel_size=3, strides=1, padding="same")
-        self.bn3 = BatchNormalization()
-        self.relu3 = Activation("relu")
-        self.pool1 = GlobalAveragePooling1D()
-
-        self.fc = Dense(classes, activation='softmax')
+        # preprocessing layers
+        tv = TextVectorization(max_tokens=vocab_size, output_mode='int', output_sequence_length=max_length)
+        tv.adapt(vocab)
+        self.pp = keras.Sequential(
+            layers=[Input(shape=(1,), dtype=tf.string),
+                    tv,
+                    Embedding(input_dim=vocab_size,output_dim=64,input_length=max_length)]
+        )
+        self.lstm = LSTM(32)
+        self.fc = Dense(classes,activation="softmax")
 
     def call(self, inputs, training=None, mask=None):
-        outputs = self.eb(inputs)
-        outputs = self.cl1(outputs)
-        outputs = self.bn1(outputs)
-        outputs = self.relu1(outputs)
-        outputs = self.cl2(outputs)
-        outputs = self.bn2(outputs)
-        outputs = self.relu2(outputs)
-        outputs = self.cl3(outputs)
-        outputs = self.bn3(outputs)
-        outputs = self.relu3(outputs)
-        outputs = self.pool1(outputs)
+        outputs = self.pp(inputs)
+        outputs = self.lstm(outputs)
         outputs = self.fc(outputs)
+        return outputs
+
+
+class CNNLayer(Layer):
+    def __init__(self, filters, **kwargs):
+        super().__init__(**kwargs)
+        self.cl = Conv1D(filters=filters, kernel_size=2, strides=1, padding="same")
+        self.bn = BatchNormalization()
+        self.relu = Activation("relu")
+
+    def call(self, inputs, *args, **kwargs):
+        outputs = self.cl(inputs)
+        outputs = self.bn(outputs)
+        outputs = self.relu(outputs)
         return outputs
